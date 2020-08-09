d168f6bc00c7c9c1818b9ac75ecaf8e9c961a4c7
Vidhan Shah <shahvidhan123@gmail.com>
2020-08-09 12:07:06 +0530

    Task 1 completed

---
+++ myFunction.js
@@ -0,0 +1,21 @@
+const myFunction = (inputList) => {
+  const myList = [];
+
+  for(let i=0;i<inputList.length;i++) {
+    const data = inputList[i].split(" ");
+    const f_name = data[0], l_name = data[1], m_marks = parseFloat(data[2]), e_marks = parseFloat(data[3]);
+
+    myList.push({
+      "Name":`${f_name} ${l_name}`,
+      "Score":{
+        "Maths": m_marks,
+        "English": e_marks
+      }
+    });
+  };
+
+  myList.sort((a,b) => (a.Score.Maths+a.Score.English)<(b.Score.Maths+b.Score.English)?1:-1);
+  console.log(myList)
+};
+
+myFunction(["Rashmil Panchani 99 97", "Parag Vaid 95 93", "Siddharth Sanghavi 98 100"]);
