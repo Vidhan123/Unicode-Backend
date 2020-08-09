37c95ce4eabc8d173121134eba15d7b66a44c88d
Vidhan Shah <shahvidhan123@gmail.com>
2020-08-09 15:36:27 +0530

    Task 2 Completed

---
+++ fetchJoke.js
@@ -0,0 +1,21 @@
+  const axios = require('axios');
+
+  let result;
+  const fetchJoke = async (Category) => {
+    try {
+      return await axios.get(`https://sv443.net/jokeapi/v2/joke/${Category}`)
+    }
+    catch (err) {
+      console.log(err)
+    }
+  };
+  
+  exports.storeJoke = async (Category) => {
+    const myData = await fetchJoke(Category);
+    const { joke,setup,id,category,error } = myData.data;
+    if (!error) {
+      const Joke = joke != null ? joke:setup;
+      result = {"category" : category , "joke" : Joke , "id" : id};
+      return result;
+    }
+  };
---
+++ jokes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const fetchJoke = require('./fetchJoke');
+const { nextTick } = require('process');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.get('/',(req,res) => {
+  res.send('Enter a joke category to get started')
+});
+
+app.get(`/:category`, async (req,res) => {
+  const result = await fetchJoke.storeJoke(req.params.category);
+  if (result!=null){
+    res.statusCode = 200;
+    res.setHeader('Content-type','application/json');
+    res.end(JSON.stringify(result));
+  }
+  else {
+    res.statusCode = 404;
+    res.setHeader('Content-type','text/html');
+    res.end('<html><body><h1>Error 404: ' + req.params.category + ' category not found');
+  }
+});
+
+
+const server = http.createServer(app);
+server.listen(port,hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`)
+});
